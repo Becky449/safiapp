1440ec3d98dd1c9c20176faebfe5e416d56d18ba
Becky449 <rabnab12@gmail.com>
2023-10-08 06:46:54 +0300

    Working User roles

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
+          enum role: [:user, :admin]
+          after_initialize :set_default_role, :if => :new_record?
+          def set_default_role
+            self.role ||= :user
+          end
+          
+          validates :firstname, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 20}
+          validates :lastname, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 20}          
+end
