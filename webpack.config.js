30616196ee4deef11f3c38ea621864f6dfddd8f4
Becky449 <rabnab12@gmail.com>
2023-11-03 14:13:35 +0300

    update devise

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,21 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './app/javascript/application.js', // Update the entry path to your Rails asset pipeline
+  entry: './app/javascript/application.js',
   devServer: {
-    static: './public', // Update the static path to your Rails public folder
-    open: true, // Automatically open the default browser
+    static: './public',
+    open: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './app/views/layouts/application.html.erb', // Update the template path to your Rails layout
-      filename: 'application.html.erb', // Output filename in your Rails app/views/layouts directory
+      template: './app/views/layouts/application.html.erb',
+      filename: 'application.html.erb',
     }),
   ],
   output: {
-    filename: 'application.js', // Output filename in your Rails app/assets/javascripts directory
-    path: path.resolve(__dirname, 'public'), // Update the output path to your Rails public folder
-    publicPath: '/public', // Public path for assets in your Rails app
+    filename: 'application.js',
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/public', 
   },
   module: {
     rules: [
